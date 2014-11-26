220ec51be3d05ad7d5595543a0a0fe9d5b07f081
Sandro <sandro0093@gmail.com>
2014-11-26 15:37:44 +0400

    paddle kedlebi

--- peddle.py
+++ peddle.py
@@ -18,9 +18,19 @@ class Peddle(cocos.sprite.Sprite):
     
         
     def update(self, keys_pressed):
-        if keys_pressed[KEY_LEFT]:
-            move = MoveBy((-80, 0), duration=0.2)
+        if keys_pressed[KEY_LEFT]:            
+            if self.x - (self.width / 2) >= 0 :
+                if self.x - (self.width / 2) > self.width / 2 :
+                    dpos = self.width / 2
+                else :
+                    dpos = self.x - (self.width / 2)
+            move = MoveBy((-dpos, 0), duration=0.07)
             self.do(move)
-        if keys_pressed[KEY_RIGHT]:
-            move = MoveBy((80, 0), duration=0.2)
-            self.do(move)        
+        elif keys_pressed[KEY_RIGHT]:
+            if (self.x + (self.width / 2)) < WINDOW_W :
+                if WINDOW_W - (self.x + (self.width / 2)) > self.width / 2 :
+                    dpos = self.width / 2
+                else :
+                    dpos = WINDOW_W - (self.x + (self.width / 2))                
+                move = MoveBy((dpos, 0), duration=0.07)
+                self.do(move)        
