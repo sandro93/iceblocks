af5561e77f58a04ff5e68d62df544a12d48e0fdd
Sandro <sandro0093@gmail.com>
2014-11-28 17:05:49 +0400

    ball collision

--- ball.py
+++ ball.py
@@ -13,20 +13,21 @@ class Ball(cocos.sprite.Sprite):
                  x=None,
                  y=None,
                  vel=None):
-        super(Ball, self).__init__(img)
+        super(Ball, self).__init__(img, anchor=(0,0))
         if x is None:
             x = WINDOW_W / 2
         if y is None:
             y = WINDOW_H / 2
         self.position = x, y
         self.dx = self.dy = BALL_SPEED
-        self.cshape = cm.AARectShape(eu.Vector2(x, y), self.width / 2, self.height / 2)
+        self.cshape = cm.AARectShape(eu.Vector2(x+(self.width/2), y+(self.height/2)), self.width / 2, self.height / 2)
 
 
-    def update(self, peddle):
-        x, y = self.cshape.center[0], self.cshape.center[1]
+    def update(self, peddle):        
+        x, y = self.position
+        
         result = 0
-        center_x = self.position[0]
+        center_x = self.cshape.center[0]
 
         peddle_x, peddle_y = math.ceil(peddle.position[0]), math.ceil(peddle.height)
 
@@ -34,8 +35,8 @@ class Ball(cocos.sprite.Sprite):
             self.dx = -1 * self.dx
         if y + self.height >= WINDOW_H:
             self.dy = -1 * self.dy
-        if y == peddle.height:
-            if (math.ceil(x) >= peddle_x and math.ceil(x) <= (peddle_x + peddle.width)) or (x + self.width >= peddle_x and x <= (peddle_x + peddle.width)):
+        if y <= peddle.height:
+            if (math.ceil(x) >= peddle_x and math.ceil(x) <= (peddle_x + peddle.width)) or (x + self.width >= peddle_x and x <= (peddle_x + peddle.width)):                
                 self.dy = -1 * self.dy
                 self.dx = (center_x - (peddle.position[0] + peddle.width/2)) / 7
             else:
@@ -47,10 +48,20 @@ class Ball(cocos.sprite.Sprite):
                 y = math.ceil(self.cshape.center[1])
                 time.sleep(1.0)
 
-        newPos = x + self.dx, y + self.dy
+        tmp_dx, tmp_dy = (self.dx, self.dy)
+        
+        if tmp_dy < 0:
+            if y + tmp_dy < peddle.height:
+                tmp_dy = (y - peddle.height) * -1
+
+        if tmp_dx < 0:
+            if x < tmp_dx:
+                tmp_dx = (x - tmp_dx) *-1
+        
+        newPos = x + tmp_dx, y + tmp_dy
         self.update_position(newPos)
         return result
 
     def update_position(self, new_position):
         self.position = new_position
-        self.cshape.center = new_position
+        self.cshape.center = new_position[0]+self.width/2, new_position[1]+self.height/2
